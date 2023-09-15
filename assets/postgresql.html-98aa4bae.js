e5da1afe3a07d3e896a7398a82c28ad3f31d3843
lijc210 <lijc210@gmail.com>
2023-09-15 01:13:51 +0000

    Deploying to gh-pages from @ lijc210/lijc210.github.io@969bd5db7a617e7831c3b96ce59b5149c8e07b49 🚀

---
+++ assets/postgresql.html-98aa4bae.js
@@ -0,0 +1,8 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as i,e as n}from"./app-534beb1c.js";const s={},t=n(`<h1 id="postgresql" tabindex="-1"><a class="header-anchor" href="#postgresql" aria-hidden="true">#</a> postgresql</h1><h2 id="关联更新" tabindex="-1"><a class="header-anchor" href="#关联更新" aria-hidden="true">#</a> 关联更新</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>UPDATE (要更新的表) table1 t1 
+SET 字段1 = t2.字段1, 字段2 = t2.字段2, 
+FROM (数据来源表) table2 t2 
+WHERE t1.key = t2.key
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="列出某个表的所有字段和描述" tabindex="-1"><a class="header-anchor" href="#列出某个表的所有字段和描述" aria-hidden="true">#</a> 列出某个表的所有字段和描述</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>select * from information_schema.columns
+where table_schema=&#39;public&#39; and table_name=&#39;tmp_ol_api_buloding_info&#39;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="将public下所有表开放所有权限给某用户" tabindex="-1"><a class="header-anchor" href="#将public下所有表开放所有权限给某用户" aria-hidden="true">#</a> 将public下所有表开放所有权限给某用户</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>GRANT ALL PRIVILEGES ON all tables in schema public TO flink;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,7),d=[t];function l(r,c){return a(),i("div",null,d)}const h=e(s,[["render",l],["__file","postgresql.html.vue"]]);export{h as default};
