97cf8c4cd87954e15fd3db9d4b1cd0f743931b0b
lijc210 <lijc210@gmail.com>
2023-12-02 13:16:27 +0000

    Deploying to gh-pages from @ lijc210/lijc210.github.io@775659fc79b3e8b1f54251acec23f46455c2d6cc 🚀

---
+++ assets/pageview-0fe96623.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app-c379c4b7.js";const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./pageview_vuepress-plugin-comment2-b25bb000.js"),[]);return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
