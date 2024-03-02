75c0a2aad46bd6032e1ddef9c1fd2690dbdb09e4
lijc210 <lijc210@gmail.com>
2024-03-02 16:19:10 +0000

    Deploying to gh-pages from @ lijc210/lijc210.github.io@08e3d0f53b01aef48fc5b34c1737aa02bd80262c 🚀

---
+++ assets/pageview-BXPd9cWd.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./app-BqWXms7F.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://zeabur.cizai.io/",pageview:!0};const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-BqWXms7F.js").then(r=>r.W),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{o as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
