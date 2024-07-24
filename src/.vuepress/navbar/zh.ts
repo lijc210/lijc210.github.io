10b4f90b0eef96bc028b85b6abb31c6bd60ba2ea
lijc210 <lijc210@163.com>
2024-07-24 11:39:45 +0800

    up

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,27 +1,5 @@
 import { navbar } from "vuepress-theme-hope";
 
-function dateFormat(fmt, date) {
-  let ret;
-  const opt = {
-    "Y+": date.getFullYear().toString(),        // 年
-    "m+": (date.getMonth() + 1).toString(),     // 月
-    "d+": date.getDate().toString(),            // 日
-    "H+": date.getHours().toString(),           // 时
-    "M+": date.getMinutes().toString(),         // 分
-    "S+": date.getSeconds().toString()          // 秒
-    // 有其他格式化字符需求可以继续添加，必须转化成字符串
-  };
-  for (let k in opt) {
-    ret = new RegExp("(" + k + ")").exec(fmt);
-    if (ret) {
-      fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
-    };
-  };
-  return fmt;
-}
-
-let sdate = dateFormat("YYYY-mm-dd", new Date())
-
 export const zhNavbar = navbar([
     "/",
   {
@@ -71,7 +49,7 @@ export const zhNavbar = navbar([
   {
     text: "每日悦读",
     icon: "read",
-    link: "/每日悦读/"
+    link: "https://nas.cizai.net/miniflux/"
   },
   {
     text: "Contact",
